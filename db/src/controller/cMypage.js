b3d5069c3e4d04d6d662217db765dfb9ad0f3b14
chh4031 <chh4031@gmail.com>
2023-11-10 02:32:48 +0900

    마이페이지 수정

--- db/src/controller/cMypage.js
+++ db/src/controller/cMypage.js
@@ -1,7 +1,17 @@
 const useDB = require("../../middleware/db")
 
 const cMypage = async(req, res) => {
-    res.render('pMypage')
+    const MemberInfo = await useDB.query(`
+    select * from 고객 where 고객아이디 = "${req.session.loginInfo.loginId}"`)
+    console.log("마이페이지 로딩 성공")
+    // console.log(MemberInfo[0][0])
+
+    res.render('pMypage', {
+        memberId : MemberInfo[0][0].고객아이디,
+        memberName : MemberInfo[0][0].고객이름,
+        memberAddress : MemberInfo[0][0].주소,
+        memberPhone: MemberInfo[0][0].휴대폰번호,
+    })
 }
 
 module.exports = {cMypage};
