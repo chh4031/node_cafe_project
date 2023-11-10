10a6019f602cdd13e31f5d782d9d53f16970cd02
chh4031 <chh4031@gmail.com>
2023-11-10 20:08:43 +0900

    주문내역 뽑기 기능 추가

--- db/src/controller/cMypage.js
+++ db/src/controller/cMypage.js
@@ -6,11 +6,17 @@ const cMypage = async(req, res) => {
     console.log("마이페이지 로딩 성공")
     // console.log(MemberInfo[0][0])
 
+    const orderInfo = await useDB.query(`
+    select * from (주문내역 inner join 메뉴항목 on 주문내역.메뉴항목_항목번호 = 메뉴항목.항목번호) inner join 주문 on 주문내역.주문_주문번호 = 주문.주문번호 where 고객_고객아이디 = "${req.session.loginInfo.loginId}" order by 주문번호 asc`)
+
+    console.log(orderInfo[0])
+
     res.render('pMypage', {
         memberId : MemberInfo[0][0].고객아이디,
         memberName : MemberInfo[0][0].고객이름,
         memberAddress : MemberInfo[0][0].주소,
         memberPhone: MemberInfo[0][0].휴대폰번호,
+        orderList : orderInfo[0]
     })
 }
 
