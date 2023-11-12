e4c7484fa782a373065d89dfef5d139cd68d5a92
chh4031 <chh4031@gmail.com>
2023-11-12 13:57:40 +0900

    검색 기능 구현 완료

--- cafe/src/controller/menuController.js
+++ cafe/src/controller/menuController.js
@@ -14,4 +14,20 @@ const menuView = async(req, res) => {
     })
 }
 
-module.exports = {menuView}
+// 검색 기능 라우터
+const searchMenu = async(req, res) => {
+
+    // search = 검색값 가져오기
+    const { search } = req.body
+
+    // 검색한 목록들 가져오기
+    const searchMenu = await useDB.query(`
+    select * from 메뉴항목 where 메뉴이름 like "%${search}%"`)
+
+    return res.render("Menu", {
+        uid : req.session.userid,
+        menuList : searchMenu[0]
+    })
+}
+
+module.exports = {menuView, searchMenu}
--- cafe/src/router/menuRouter.js
+++ cafe/src/router/menuRouter.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const menuRouter = require('../controller/menuController');
 
+// 메뉴 목록 라우터
 router.get("/", menuRouter.menuView);
+
+// 검색 기능 라우터
+router.post("/search", menuRouter.searchMenu);
 
 module.exports = router;
